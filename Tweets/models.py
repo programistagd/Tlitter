f485ff7a1a1bfd52f70a37596088621a2a08ad66
Radosław Waśko <programistagd@gmail.com>
2017-11-07 14:56:15 +0100

    Added "Follows" relation.

--- Tweets/models.py
+++ Tweets/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+
 class Profile(models.Model):
-    nickname = models.CharField(max_length=32, unique=True)
+    nickname = models.CharField(max_length=32, unique=True) #TODO add index here
     about = models.CharField(max_length=140)
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
 
@@ -12,9 +13,17 @@ class Profile(models.Model):
 
 
 class Tweet(models.Model):
-    poster = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    poster = models.ForeignKey(Profile, on_delete=models.CASCADE) #TODO add index here
     text = models.CharField(max_length=140)
-    pub_date = models.DateTimeField('post date', default=timezone.now)
+    pub_date = models.DateTimeField("post date", default=timezone.now)
 
     def __str__(self):
         return self.text
+
+
+class Following(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="following")
+    target = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="followers")
+
+    def __str__(self):
+        return str(self.follower) + " -> " + str(self.target)
