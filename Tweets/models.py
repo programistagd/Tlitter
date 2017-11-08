b3df5712b02619cddfce3e6132a82295d04ec907
Radosław Waśko <programistagd@gmail.com>
2017-11-09 00:14:05 +0100

    Added indexes to Profile and Tweet.

--- Tweets/models.py
+++ Tweets/models.py
@@ -6,14 +6,14 @@ from django.conf import settings
 class Profile(models.Model):
     nickname = models.CharField(max_length=32, unique=True, db_index=True) #TODO add index here
     about = models.CharField(max_length=140)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, db_index=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return self.nickname
 
 
 class Tweet(models.Model):
-    poster = models.ForeignKey(Profile, on_delete=models.CASCADE, db_index=True) #TODO add index here
+    poster = models.ForeignKey(Profile, on_delete=models.CASCADE) #TODO add index here
     text = models.CharField(max_length=140)
     pub_date = models.DateTimeField("post date", default=timezone.now, db_index=True)
 
